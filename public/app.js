1921b5cb21c7c6ec3e79173617bd3c0f45d64acd
Sean Murray <thecanbeat@gmail.com>
2016-09-13 13:58:23 -0600

    added user account system

--- public/app.js
+++ public/app.js
@@ -1,14 +1,59 @@
+// Sockets are easier than writing a bunch of http requests.
+var socket = new WebSocket(`ws://${location.hostname}:4261/ws`);
+
+// Memoization for DOM element lookups.
+var dom = {
+  auth: el(".auth")[0],
+  authButton: el(".auth-button")[0],
+  userInput: el("#user-input"),
+  passInput: el("#pass-input"),
+  uid: el("#uid"),
+  loc: el("#loc")
+};
+
 // Browser Fingerprint
 new Fingerprint2({extendedFontList: true}).get((result, components) => {
+  // Refreshes if this isn't the same id you got last time.
+  // For some reason if you restart a browser the first visit gives you a different id than every subsequent visit.
   if(result !== localStorage.last){
     localStorage.last = result;
     return location.reload();
   }
-  document.getElementById("uid").textContent = result;
+  dom.uid.textContent = result;
+  login();
 });
 
-// Location Fingerprint
-var socket = new WebSocket(`ws://${location.hostname}:4261/ws`);
+// Location and User Fingerprint
 socket.onmessage = data => {
-  document.getElementById("loc").textContent = data.data;
+  data = JSON.parse(data.data);
+  if(data.type === "loc") dom.loc.textContent = data.data;
+  if(data.type === "user") loadUser(data.data);
+}
+
+function openAuth(){
+  if(dom.authButton.textContent === "Account") dom.auth.classList.toggle("left300px");
+}
+
+function login(){
+  var obj = {uid: localStorage.last};
+  if(dom.userInput.value && dom.passInput.value){
+    obj.username = dom.userInput.value;
+    obj.password = dom.passInput.value;
+    dom.userInput.value = "";
+    dom.passInput.value = "";
+  }
+  socket.send(JSON.stringify(obj));
+}
+
+function loadUser(data){
+  dom.authButton.classList.add("accounted");
+  dom.authButton.textContent = data.username;
+}
+
+// Utility
+function el(name, parent){
+  parent = parent || document;
+  if(name[0] === ".") return parent.getElementsByClassName(name.substr(1));
+  if(name[0] === "#") return parent.getElementById(name.substr(1));
+  return parent.getElementsByTagName(name);
 }
--- server.js
+++ server.js
@@ -1,19 +1,55 @@
 var server = require("http").createServer(),
   crypto = require("crypto")
+  fs = require("fs"),
   WebSocketServer = require("ws").Server,
   wss = new WebSocketServer({server: server}),
   express = require("express"),
-  app = express();
+  app = express(),
+  users = require(__dirname + "/db/users.json");
 
 app.use(express.static(__dirname + "/public"));
 
 wss.on("connection", ws => {
-  ws.send(hmac(ws._socket.remoteAddress));
+  ws.send(JSON.stringify({type: "loc", data: hmac(ws._socket.remoteAddress)}));
+  ws.on("message", data => {
+    data = JSON.parse(data);
+    var user = users.find(e => e.browsers.indexOf(data.uid) > -1);
+    if(user) return sendUser(ws, user);
+    if(data.username) user = users.find(e => e.username === data.username);
+    if(!user && data.username && data.password){
+      user = createUser(data.username, data.password, data.uid);
+    }
+    if(user && user.hash === hmac(data.password)){
+      if(user.browsers.indexOf(data.uid) === -1){
+        user.browsers.push(data.uid);
+        saveUsers();
+      }
+      return sendUser(ws, user);
+    }
+    if(!user) ws.send(JSON.stringify({type: "user", data: false}));
+  });
 });
 
 server.on('request', app);
 server.listen(4261);
 
+function saveUsers(){
+  fs.writeFile(__dirname + "/db/users.json", JSON.stringify(users));
+}
+
+function sendUser(ws, user){
+  console.log(user);
+  ws.send(JSON.stringify({type: "user", data: {username: user.username, browsers: user.browsers}}));
+}
+
+function createUser(username, password, uid){
+  var user = {username, hash: hmac(password), browsers: [uid]};
+  users.push(user);
+  saveUsers();
+  return user;
+}
+
 function hmac(data){
+  if(!data) return "";
   return crypto.createHmac("sha256", "lazy_secret").update(data).digest("hex").slice(0, 30);
 }
